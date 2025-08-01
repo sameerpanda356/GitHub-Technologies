301f522f0d21715849a937db87405b0b4b72243e
sameerpanda356 <sameerpanda356@gmail.com>
2025-08-01 19:31:31 +0200

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -145,7 +145,7 @@ const Footer: React.FC<FooterProps> = ({ setCurrentPage }) => {
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
           <button
-            onClick={() => setCurrentPage('privacy-policy')}
+            onClick={() => setCurrentPage('privacy-policy')} 
             className="text-gray-400 hover:text-white text-sm transition-colors"
           >
             Privacy Policy
